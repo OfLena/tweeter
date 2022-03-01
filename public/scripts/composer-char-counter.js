f330beb91fb86097054b9b327569cf2971391ec2
Guy Dilena <guydilena@gmail.com>
2022-03-01 19:15:20 -0400

    Add Feature: Nav Bar is now fixed to the top of the page.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -23,30 +23,10 @@ $("#tweet-text").on('input', function () {
   } else {
     counter.removeClass('change-tweet-counter');
   };
-
-
-    
-
   return counter;
 });
 });
-// $("#tweet-text").on('keydown', function() {
-//   console.log(this); //The this keyword is a reference to the button
-// });
 
-// $("#tweet-text").on('input', () => {
-//   console.log(this); //The this keyword here refers to something else!
-// });
-
-
-/* <form action="/tweets/" method="POST">
-<label for="tweet-text" class="humming-about">What are you humming about?</label>
-<textarea name="text" id="tweet-text"></textarea>
-<div class="under-tweet-box">
-  <button class="tweet-btn" type="submit">Tweet</button>
-  <output name="counter" class="tweet-text-counter" for="tweet-text">140</output>
-</div>
-</form> */
 
 
 
