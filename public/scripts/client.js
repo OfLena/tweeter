55ea4009e4f8bd61089c1f698b1b71b00e319de8
Guy Dilena <guydilena@gmail.com>
2022-03-02 10:32:52 -0400

    Feature: added the mock tweets to the bottom of our Tweeter page.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-  console.log('Testing Document Ready')
-});
+
+
   
-  const renderTweets = function(tweets) {
-    for (const tweet of tweets) {
-      
-    }
-  }
-
-
-  const createTweetElement = function(tweet) {
-
-    let $tweet =   $(`<section class="all-tweets" id="tweet-container">
-    <article class="single-tweet">
-      <header><span><i class="fa-solid fa-user-astronaut"></i>${tweetData.user.name}</span><span>${tweetData.user.handle}</span></header>
-      <p class="posted-tweet">${tweetData.content.text}</p>
-      <footer><span class="footer-text">${timeago.format(tweetData.created_at)}</span><span><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></span></span></footer>
-    </article>
-    </section>
-  `)
-
-    return $tweet
-}
-
-
 const data = [
   {
     "user": {
@@ -56,9 +33,38 @@ const data = [
 ]
 
 
-const $tweet = createTweetElement(data);
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to ad
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      console.log(tweet);
+      $('#tweet-container').append(createTweetElement(tweet));
+    }
+  }
+
+
+
+  const createTweetElement = function(tweet) {
+
+    let $tweet =   $(`
+    <article class="single-tweet">
+      <header><span><img class="name-icon" src=${tweet.user.avatars}>${tweet.user.name}</span><span>${tweet.user.handle}</span></header>
+      <p class="posted-tweet">${tweet.content.text}</p>
+      <footer><span class="footer-text">${timeago.format(tweet.created_at)}</span><span><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></span></span></footer>
+    </article>
+  `)
+
+    return $tweet
+}
+
+$(document).ready(function() {
+  console.log('Testing Document Ready')
+renderTweets(data);
+});
+
+
+
+// const $tweet = createTweetElement(data);
+// console.log($tweet);
+// $('#tweets-container').append($tweet);
 
 
 
