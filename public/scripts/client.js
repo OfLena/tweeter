532f649905650db3fb20f05aef34d385c0dea7d1
Guy Dilena <guydilena@gmail.com>
2022-03-02 15:44:44 -0400

    Improve/Change: server now stops redirection on form submit. Takes form input as datatype JSON and posts, with AJAX, to the /tweets page.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,7 +6,8 @@
 
 
 
-  
+
+
 const data = [
   {
     "user": {
@@ -32,16 +33,15 @@ const data = [
   }
 ]
 
+//////////////////**********RENDER TWEETS***********/////////////////////
 
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
-      console.log(tweet);
+      // console.log(tweet);
       $('#tweet-container').prepend(createTweetElement(tweet));
     }
   }
-
-
-
+/////////////////************GRAB INFO FROM INDIVDUAL TWEETS FOR RENDER***********/////////////
   const createTweetElement = function(tweet) {
 
     let $tweet =   $(`
@@ -51,11 +51,34 @@ const data = [
       <footer><span class="footer-text">${timeago.format(tweet.created_at)}</span><span><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></span></span></footer>
     </article>
   `)
-
     return $tweet
-}
+};
+
+//////////////***********RENDER TWEETS IN THE DOM***********///////////////
 
 $(document).ready(function() {
+
+  const $form = $("#new-tweet");
+$form.on("submit", (event) => {
+  event.preventDefault();
+  //console.log to make sure that the submit button is being diverted.
+  console.log("The Form was submitted");
+  const serializedData = $(event.target).serialize();
+  //check to see if the serializedData is correct.
+  console.log(serializedData);
+
+  $.ajax({
+    type: "POST",
+    url: "/tweets",
+    data: serializedData,
+    dataType: "json"
+  })
+
+  // $.post("/tweets", serializedData, () => {
+  //   console.log(response);
+  // })
+})
+
   console.log('Testing Document Ready')
 renderTweets(data);
 });
