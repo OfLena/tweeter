5f796ea294ec75b1406245c4a2680b580fc09981
Guy Dilena <guydilena@gmail.com>
2022-03-03 18:03:46 -0400

    Feature: Added a feature that when clicked scrolls you back to the top of the page.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,21 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
 //////////////////**********RENDER TWEETS***********/////////////////////
 
 const renderTweets = function(tweets) {
-  $('#tweet-container').empty()
+  $('#tweet-container').empty();
   for (const tweet of tweets) {
     // console.log(tweet);
     $('#tweet-container').prepend(createTweetElement(tweet));
   }
 };
-/////////////////************GRAB INFO FROM INDIVDUAL TWEETS FOR RENDER***********/////////////
+
+/////////////*********GRAB INFO FROM INDIVDUAL TWEETS FOR RENDER***********/////////////
+
 const createTweetElement = function(tweet) {
 
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
@@ -33,11 +33,6 @@ const createTweetElement = function(tweet) {
   `);
   return $tweet;
 };
-
-//////////////***********GET COUNTER VARIABLE***************//////////////
-
-
-
 
 //////////////***********RENDER TWEETS IN THE DOM***********///////////////
 
@@ -59,32 +54,44 @@ $(document).ready(function() {
   const $form = $("#new-tweet");
   $form.on("submit", (event) => {
     event.preventDefault();
-    const counter = $("textarea").val().length
+    const counter = $("textarea").val().length;
     
     if (counter > 0 && counter < 140) {
-      $('#slide-down-warning').slideUp("slow")
-     }
+      $('#slide-down-warning').slideUp("slow");
+    }
     if (counter === 0 || counter > 140) {
-     $('#slide-down-warning').text(`⛔️ Error: Tweet Must Be Between 0 and 140 Characters ⛔️`).slideDown("slow")
-    return
+      $('#slide-down-warning').text(`⛔️ Error: Tweet Must Be Between 0 and 140 Characters ⛔️`).slideDown("slow");
+      return;
     }
     
-
-    //console.log to make sure that the submit button is being diverted.
     console.log("The Form was submitted");
+
     const serializedData = $(event.target).serialize();
     $.ajax({
       type: "POST",
       url: "/tweets",
       data: serializedData,
-      })
-      .then(() => {
-      loadTweets()
-      $('textarea').val('');
-      $('.tweet-text-counter').val('140');
-      })
-      .catch(err => {console.log('err', err)
     })
+      .then(() => {
+        loadTweets();
+        $('textarea').val('');
+        $('.tweet-text-counter').val('140');
+      })
+      .catch(err => {
+        console.log('err', err);
+      });
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+  });
+  
+  $("#toTop").click(function() {
+    $("html, body").animate({scrollTop: 0}, 1000);
   });
 
   console.log('Testing Document Ready');
